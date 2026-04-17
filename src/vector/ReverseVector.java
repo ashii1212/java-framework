46c19b0c3ebd3d89a637340f1c15d642867a641e
abdul <abdulashiqsk2003@gmail.com>
2026-04-18 01:55:32 +0530

    add class reverse a vector using Collection and manual reverse

---
+++ src/vector/ReverseVector.java
@@ -0,0 +1,42 @@
+package vector;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class ReverseVector {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        Vector<Integer> data = new Vector<>();
+        for(int i=1;i< 10;i++)
+        {
+            System.out.println("Enter a values of "+i+" :");
+            int n = sc.nextInt();
+            data.add(n);
+        }
+        System.out.println("=================Approach 1 without using Collections==========================");
+        System.out.println("Reverse :"+reverse(data));
+        System.out.println("==================Approach 1 without using Collections=========================");
+        System.out.println("Reverse :"+reverseNum(data));
+    }
+// Approach 1 without using Collections
+    public static  Vector<Integer> reverse(Vector<Integer> data)
+    {
+        Vector<Integer> rev = new Vector<>();
+
+         for(int i=data.size(); i>0;i--)
+         {
+             rev.add(i);
+         }
+
+         return  rev;
+    }
+// Approach 2 with  using Collections
+    public static  Vector<Integer> reverseNum(Vector<Integer> data)
+    {
+        Collections.reverse(data);
+        return data;
+    }
+}
