79e0cca64d1cfde44e39d3c936aeed62c7d0e7a5
abdul <abdulashiqsk2003@gmail.com>
2026-04-18 19:07:58 +0530

    Added a class with elements and printed the elements

---
+++ src/arraylist/PrintElements.java
@@ -0,0 +1,28 @@
+package arraylist;
+
+import java.util.ArrayList;
+
+public class PrintElements {
+    public static void main(String[] args) {
+        ArrayList<Integer> data = new ArrayList<>();
+        data.add(1);
+        data.add(4);
+        data.add(7);
+        data.add(9);
+        data.add(10);
+        data.add(2);
+        data.add(15);
+        data.add(20);
+        data.add(25);
+        data.add(30);
+        System.out.println("Using For-Loop");
+        for(int i=0;i<data.size();i++){
+            System.out.println(data.get(i));
+        }
+
+        System.out.println("Using For-Each Loop");
+        for(Integer num : data){
+            System.out.println(num);
+        }
+    }
+}
